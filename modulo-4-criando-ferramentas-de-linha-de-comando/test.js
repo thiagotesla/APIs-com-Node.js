c204e1a6903421a0b90aeaa4d96ce469235316f6
Thiago Barros <88802518+thiagotesla@users.noreply.github.com>
2022-04-28 16:18:06 -0300

    remove before desnecessario

--- modulo-4-criando-ferramentas-de-linha-de-comando/test.js
+++ modulo-4-criando-ferramentas-de-linha-de-comando/test.js
@@ -14,18 +14,18 @@ const DEFAULT_ITEM_CADASTRAR = {
 }
 
 describe('Suite de manipulação de Herois', () => {
-    before('Cadastrar um heroi', async () => {
-        await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
-    })
+
     it('CREATE: Deve cadastrar um heroi usando arquivos.', async () => {
         const expected = DEFAULT_ITEM_CADASTRAR
         const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
         ok(resultado, expected)
     })
+
     it('READ: Deve pesquisar um heroi usando arquivos.', async () => {
         const expected = DEFAULT_ITEM_CADASTRAR
         const [resultado] = await database.listar(expected.id)
 
         deepEqual(resultado, expected)
     })
+
 })
