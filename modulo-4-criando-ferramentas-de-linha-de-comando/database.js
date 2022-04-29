b1b747ba9e91aa05ace705996f6463b03f4cf21e
Thiago Barros <88802518+thiagotesla@users.noreply.github.com>
2022-04-29 11:32:11 -0300

    refatora const id

--- modulo-4-criando-ferramentas-de-linha-de-comando/database.js
+++ modulo-4-criando-ferramentas-de-linha-de-comando/database.js
@@ -32,7 +32,7 @@ class Database{
     }
     async cadastrar(heroi){
         const dados = await this.obterDadosArquivo()
-        const id = heroi.id <=2 ? heroi.id : Date.now()
+        const id = Date.now()
         const heroiComId = {
             id,
             ...heroi
