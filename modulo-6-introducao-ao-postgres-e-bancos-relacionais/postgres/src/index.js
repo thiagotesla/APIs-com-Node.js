c359e5f616d16e0c7a55c58f7dc9df1a9b8b1524
Thiago Barros <88802518+thiagotesla@users.noreply.github.com>
2022-04-29 23:11:00 -0300

    altera imports path

--- modulo-6-introducao-ao-postgres-e-bancos-relacionais/postgres/src/index.js
+++ modulo-6-introducao-ao-postgres-e-bancos-relacionais/postgres/src/index.js
@@ -1,6 +1,6 @@
-const ContextEstrategy = require('./db/strategies/base/conxtextStrategy')
-const MongoDb = require('./db/strategies/mongodb')
-const Postgres = require('./db/strategies/postgres')
+const ContextEstrategy = require('./db/strategies/base/context.strategy')
+const MongoDb = require('./db/strategies/mongodb.strategy')
+const Postgres = require('./db/strategies/postgres.strategy')
 
 const ContextMongo = new ContextEstrategy(new MongoDb())
 ContextMongo.create()
