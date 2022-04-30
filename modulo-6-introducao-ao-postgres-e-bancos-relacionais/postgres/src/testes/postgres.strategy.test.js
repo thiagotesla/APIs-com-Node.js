2b7b420b52c5eed241a081b9b6c79c3979e6ad79
Thiago Barros <88802518+thiagotesla@users.noreply.github.com>
2022-04-29 23:10:33 -0300

    cria teste unitario de conexao com o database

---
+++ modulo-6-introducao-ao-postgres-e-bancos-relacionais/postgres/src/testes/postgres.strategy.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert')
+const Postgres = require('../db/strategies/postgres.strategy')
+const Context = require('../db/strategies/base/context.strategy')
+
+const context = new Context(new Postgres())
+
+describe('Postgres Strategy', function () {
+    this.timeout(Infinity)
+    it('Should grant PostgreSQL connection', async () =>{
+        const result = await context.isConnected()
+        assert.equal(result, true)
+    })
+})
