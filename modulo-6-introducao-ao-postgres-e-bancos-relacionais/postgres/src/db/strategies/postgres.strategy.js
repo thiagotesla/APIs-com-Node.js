dcd6a04711d610b6b7d070b119da4457a2464004
Thiago Barros <88802518+thiagotesla@users.noreply.github.com>
2022-05-01 15:34:05 -0300

    adiciona metodo update ao postgres strategy

--- modulo-6-introducao-ao-postgres-e-bancos-relacionais/postgres/src/db/strategies/postgres.strategy.js
+++ modulo-6-introducao-ao-postgres-e-bancos-relacionais/postgres/src/db/strategies/postgres.strategy.js
@@ -49,6 +49,9 @@ class Postgres extends ICrud {
         return await this._herois.findAll({where: item, raw: true} )
     }
 
+    async update(id, item){
+        return await this._herois.update(item, {where: {id: id}})
+    }
     async connect() {
         this._driver = new Sequelize(
             'heroes',
